226898d7e07f2f4ef884b9b42748b355dada957f
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2021-06-02 23:27:29 +0500

    Update AddTransaction.js

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const AddTransaction = () => {
+    const [description, setDescription]=useState();
+    const [transactionAmount, setTransactionAmount]=useState("")
     return (
         <div>
              <h3>Add New Transaction</h3>
@@ -9,13 +11,17 @@ export const AddTransaction = () => {
                      <label htmlFor="description">
                          description
                      </label>
-                     <input type="text" id="description" placeholder="Detail of Transaction" />
+                     <input type="text" id="description" placeholder="Detail of Transaction"
+                     onChange={(e)=>setDescription(e.target.value)}
+                     value={description} />
                  </div>
                  <div className="form-control">
                      <label htmlFor="transactionamount">
                          Transaction Amount
                      </label>
-                     <input type="number" id="transactionamount" placeholder="Detail of Transaction" />
+                     <input type="number" id="transactionamount" placeholder="Detail of Transaction" 
+                     onChange={(e)=>setTransactionAmount(e.target.value)} 
+                     value={transactionAmount} />
                  </div>
                  <button className="btn">
                      Add Transaction
